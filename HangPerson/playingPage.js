0181dbc5a316af0da488cd57f386f1993160fe7e
ashleighmjohnson <123028785+ashleighmjohnson@users.noreply.github.com>
2024-10-09 18:29:37 -0500

    -updated stuff
    -updates

---
+++ HangPerson/playingPage.js
@@ -0,0 +1,27 @@
+// // variables
+// const wordDisplay = document.querySelector(".word-display");
+// const guessesTxt = document.querySelector(".guesses-Txt");
+// const keyboardDiv = document.querySelector(".keyboard");
+// const hangmanPic = document.querySelector(".hangmanBox-Pic");
+// const gameModel = document.querySelector(".game-Model");
+// const playAgainButt = gameModel.querySelector(".playAgaing-Butt");
+
+// // initializing 
+
+// let currentWord, correctLetters, wrongGuessCount;
+// const maxGuess = 6;
+
+// const resetGame = () => {
+//     correctLetters = [];
+//     wrongGuessCount = 0;
+//     hangmanPic.src = "hangman-0.svg";
+//     guessesTxt.innerText = `${wrongGuessCount} / ${maxGuess}`;
+//     // create empty letter dashes 
+//     wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li`).join("");
+//     // enable keyboard buttons
+//     keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+//     // hide game model
+//     gameModel.classList.remove("show");
+// }
+
+// resetGame(); 
