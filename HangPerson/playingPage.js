93b0217ee285e20ca3e9a395e7f4237856b69b65
Rmclaughlin2022 <ryan.mclaughlin2022@gmail.com>
2024-10-28 03:43:31 -0500

    27:01 video
    I dont know whats going on with the max guesses not being able to show up in the console ill look at it tmr

--- HangPerson/playingPage.js
+++ HangPerson/playingPage.js
@@ -1,27 +1,50 @@
-// variables
 const wordDisplay = document.querySelector(".word-display");
-const guessesTxt = document.querySelector(".guesses-Txt");
 const keyboardDiv = document.querySelector(".keyboard");
+const guessesText = document.querySelector(".guesses-text b");
 const hangmanPic = document.querySelector(".hangmanBox-Pic");
 const gameModel = document.querySelector(".game-Model");
-const playAgainButt = gameModel.querySelector(".playAgaing-Butt");
+/*const playAgainButt = gameModel.querySelector(".playAgaing-Butt");*/
 
-// initializing 
-
-let currentWord, correctLetters, wrongGuessCount;
+let currentWord, wrongGuessCount = 0;
 const maxGuess = 6;
 
-const resetGame = () => {
+/*const resetGame = () => {
     correctLetters = [];
     wrongGuessCount = 0;
     hangmanPic.src = "hangman-0.svg";
-    guessesTxt.innerText = `${wrongGuessCount} / ${maxGuess}`;
-    // create empty letter dashes 
-    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li`).join("");
-    // enable keyboard buttons
-    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
-    // hide game model
-    gameModel.classList.remove("show");
+    guessesTxlt.innerText = `${wrongGuessCount} / ${maxGuess}`;
 }
 
 resetGame(); 
+*/
+const getRandomWord = () => {
+    const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    currentWord = word;
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => '<li class="letter"></li>').join("");
+}
+
+const initGame = (button, clickedLetter) => {
+    if(currentWord.includes(clickedLetter)) {
+        console.log(clickedLetter, "does exist on the word");
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+    } else {
+        wrongGuessCount++;
+    }
+    guessesText.innerText = {wrongGuessCount} /  {maxGuesses};
+}
+
+for (let i = 97; i<= 122; i++){
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
